95d8813bc4090766ccbd2fd690a456978d7a55bc
Franz Luther Neulist Carroll <franzneulistcarroll@gmail.com>
2016-06-04 22:52:01 +1000

    added a Javadoc example for CI

--- core/src/com/conductor/game/ConductorGame.java
+++ core/src/com/conductor/game/ConductorGame.java
@@ -6,6 +6,9 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+/**
+ * This is a test javadoc
+ */
 public class ConductorGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
