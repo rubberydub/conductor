22d3080cb4647914a096160e0f2793eccd996975
Franz Luther Neulist Carroll <franzneulistcarroll@gmail.com>
2016-06-04 20:31:34 +1000

    configured gradle for unit tests, unit test stub

---
+++ core/test/com/conductor/game/ExampleTest.java
@@ -0,0 +1,22 @@
+package com.conductor.game;
+import org.junit.Ignore;
+import org.junit.Test;
+
+
+import static org.junit.Assert.assertTrue;
+
+
+public class ExampleTest 
+{
+    @Test
+    public void thisAlwaysPasses() 
+    {
+        assertTrue(true);
+    }
+
+    @Test
+    @Ignore
+    public void thisIsIgnored() 
+    {
+    }
+}
