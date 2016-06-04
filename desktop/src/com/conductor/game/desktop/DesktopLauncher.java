e1e3199859b0a22fa1075ced3c2499b9b4d77496
Franz Luther Neulist Carroll <franzneulistcarroll@gmail.com>
2016-06-04 20:13:41 +1000

    initial commit with fresh libGDX install

---
+++ desktop/src/com/conductor/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.conductor.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.conductor.game.ConductorGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ConductorGame(), config);
+	}
+}
